db7696c2e01ca3e339f67b7bef368d16ab07d5f9
Ante Budimir <abudimir85@gmail.com>
2021-07-01 10:55:01 +0200

    sharing links added

--- js/main.js
+++ js/main.js
@@ -1,43 +1,3 @@
-// Register SW
-if ('serviceWorker' in navigator) {
-	// register him
-	navigator.serviceWorker
-		.register('/sw.js', {
-			updateViaCache: 'none',
-			scope: '/',
-		})
-		.then(() => {
-			// finished registering
-		})
-		.catch((err) => {
-			console.warn('Failed to register', err.message);
-		});
-
-	// listen for messages
-	navigator.serviceWorker.addEventListener('message', ({ data }) => {
-		// received a message from the service worker
-		console.log(data, 'New message from your service worker.');
-	});
-}
-
-// SYNC
-async function registerPeriodicCheck() {
-	const registration = await navigator.serviceWorker.ready;
-	try {
-		await registration.periodicSync.register('latest-update', {
-			minInterval: 24 * 60 * 60 * 1000,
-		});
-	} catch {
-		console.log('Periodic sync could not be registered!');
-	}
-}
-
-navigator.serviceWorker.ready.then((registration) => {
-	registration.periodicSync.getTags().then((tags) => {
-		if (tags.includes('latest-update')) skipDownloadingLatestUpdateOnPageLoad();
-	});
-});
-
 // Hamburger
 if (window.innerWidth < 1024) {
 	const hamburger = document.querySelector('#hamburger');
@@ -96,21 +56,52 @@ if (document.body === galleryHome) {
 const europeanaHome = document.querySelector('#europeana-home');
 
 if (document.body === europeanaHome || document.body === galleryHome) {
-	const scrollToTop = document.querySelector('.scrollButton');
+	const scrollToTop = document.querySelector('.scrollButton'),
+		sharingLinks = document.querySelector('#sharing-links');
 
 	window.addEventListener('scroll', () => {
+		// scrollToTop button
 		if (
-			document.body.scrollTop > 1000 ||
-			document.documentElement.scrollTop > 1000
+			document.body.scrollTop > 1500 ||
+			document.documentElement.scrollTop > 1500
 		) {
 			scrollToTop.style.display = 'block';
 		} else {
 			scrollToTop.style.display = 'none';
+		}
+
+		// sharingLinks button
+		if (
+			document.body.scrollTop > 1000 ||
+			document.documentElement.scrollTop > 1000
+		) {
+			sharingLinks.style.display = 'flex';
+		} else {
+			sharingLinks.style.display = 'none';
 		}
 	});
 
 	scrollToTop.addEventListener('click', () => {
 		document.body.scrollTop = 0;
 		document.documentElement.scrollTop = 0;
+	});
+}
+
+// Sharing links
+const mapHome = document.querySelector('#map-home');
+
+if (
+	document.body === mapHome ||
+	document.body === galleryHome ||
+	document.body === europeanaHome
+) {
+	const toggle = document.querySelector('.toggle');
+
+	toggle.addEventListener('click', () => {
+		const sharingLinks = document.querySelectorAll('.sharing-link');
+
+		for (let i = 0; i < sharingLinks.length; i++) {
+			sharingLinks[i].classList.toggle('open');
+		}
 	});
 }
